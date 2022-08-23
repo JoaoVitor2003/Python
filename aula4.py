9349b6c3ac13f108acca139ac6d3cd55f31d3cc2
João Vitor de Oliveira <88735946+JoaoVitor2003@users.noreply.github.com>
2022-08-23 16:12:40 -0300

    Operações

---
+++ aula4.py
@@ -0,0 +1,29 @@
+#operador atribuição
+nome= 'angela'
+
+#operador aritméticos
+soma = 45 + 38
+subtração = 70 - 16
+multiplicação = 67 * 3
+divisão = 40 / 5
+parteinteira = 10 // 3
+resto = 10 % 3
+potencia = 2**3
+
+print ('a soma é: {}'.format (soma))
+print (f'a subtração é:{subtração}')
+print(f'a multiplicação é: ',multiplicação)
+print (f'a divisão é:{divisão}')
+print (f'a parte inteira',parteinteira)
+print ('resto da divisão é:',resto)
+print ('potencia é:',potencia)
+
+print ('------------------------------------------------------------------')
+
+
+massa = input ('digite sua massa:')
+print (type(massa))
+
+
+altura = input ('digite sua altura')
+print (type(altura))
