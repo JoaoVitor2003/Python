98861ff67ef10e524ebb13581f3c2fe4a5295de1
João Vitor de Oliveira <88735946+JoaoVitor2003@users.noreply.github.com>
2022-08-25 19:13:19 -0300

    For

---
+++ Exemplo2_For.py
@@ -0,0 +1,19 @@
+
+
+for n in range(10):
+    print(n)
+
+print('-'*80)
+
+for n in range(5,10):
+    print(n)
+
+print('-'*80)
+
+for n in range(0, 102, 2):
+    print(n)
+
+print('-'*80)
+
+for n in range(100, -1, -1):
+    print(n)
